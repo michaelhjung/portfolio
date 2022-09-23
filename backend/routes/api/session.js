6e1411fb426b2a5fb47c9f568e4258ef9cf3693f
Michael Jung <jung.michaelh@gmail.com>
2022-09-23 10:24:56 -0700

    Add boilerplate

---
+++ backend/routes/api/session.js
@@ -0,0 +1,75 @@
+const express = require('express')
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+/* ------------------------------- MIDDLEWARE ------------------------------- */
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .withMessage('Email or username is required')
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Password is required'),
+    handleValidationErrors
+];
+
+/* --------------------------------- ROUTES --------------------------------- */
+// Log in
+router.post(
+    '/',
+    validateLogin,
+    async (req, res, next) => {
+        const { credential, password } = req.body;
+
+        const user = await User.login({ credential, password });
+
+        if (!user) {
+            const err = new Error('Invalid credentials');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
+
+        const jwtToken = await setTokenCookie(res, user);
+
+        const userData = {};
+        userData.id = user.id;
+        userData.firstName = user.firstName;
+        userData.lastName = user.lastName;
+        userData.email = user.email;
+        userData.username = user.username;
+        userData.token = jwtToken;
+
+        return res.json({ ...userData });
+    }
+);
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) return res.json(user.toSafeObject());
+        else return res.json(null);
+    }
+);
+
+module.exports = router;
---
+++ backend/routes/api/users.js
@@ -0,0 +1,88 @@
+const express = require('express')
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+/* ------------------------------- MIDDLEWARE ------------------------------- */
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .withMessage('Invalid email')
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .withMessage('First Name is required')
+        .isLength({ min: 2 })
+        .withMessage('Please provide a first name with at least 2 characters.'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .withMessage('Last Name is required')
+        .isLength({ min: 2 })
+        .withMessage('Please provide a last name with at least 2 characters.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .withMessage('Username is required')
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Password is required')
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+/* --------------------------------- ROUTES --------------------------------- */
+// Sign up
+router.post(
+    '/',
+    validateSignup,
+    async (req, res, next) => {
+        const { firstName, lastName, email, password, username } = req.body;
+
+        const existingEmail = await User.findOne({ where: { email } });
+        const existingUsername = await User.findOne({ where: { username } });
+
+        if (existingEmail) {
+            const err = new Error("User already exists");
+            err.status = 403;
+            err.errors = { "email": "User with that email already exists"}
+            next(err);
+        }
+        else if (existingUsername) {
+            const err = new Error("User already exists");
+            err.status = 403;
+            err.errors = { "username": "User with that username already exists"}
+            next(err);
+        }
+        else {
+            const user = await User.signup({ firstName, lastName, email, username, password });
+
+            const jwtToken = await setTokenCookie(res, user);
+
+            const userData = {};
+            userData.id = user.id;
+            userData.firstName = user.firstName;
+            userData.lastName = user.lastName;
+            userData.email = user.email;
+            userData.username = user.username;
+            userData.token = jwtToken;
+
+            return res.json({
+                ...userData
+            });
+        }
+    }
+);
+
+
+module.exports = router;
