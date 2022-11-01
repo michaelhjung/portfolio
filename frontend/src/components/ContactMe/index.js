478eec4412a419771954f6ea933d286856b9f3b7
Michael Jung <jung.michaelh@gmail.com>
2022-11-01 14:27:06 -0700

    Update contact me, simplify projects animation

--- frontend/src/components/ContactMe/index.js
+++ frontend/src/components/ContactMe/index.js
@@ -1,9 +1,19 @@
 import "./ContactMe.css"
+import linkedin from '../../assets/icons/LinkedIn.svg';
+import github from '../../assets/icons/Github-Dark.svg';
 
 export default function ContactMe() {
     return (
         <div className="contact-me-container" >
             <h2>Contact Me</h2>
+            <div className="contact-me-icons-container">
+                <a className="contact-icon" href="https://www.linkedin.com/in/michael-h-jung/" target="_blank" rel="noreferrer">
+                    <img className="contact-icon-img" src={linkedin} alt="linked in" />
+                </a>
+                <a className="contact-icon" href="https://github.com/michaelhjung" target="_blank" rel="noreferrer">
+                    <img className="contact-icon-img" src={github} alt="github" />
+                </a>
+            </div>
         </div>
     )
 }
--- frontend/src/components/Projects/index.js
+++ frontend/src/components/Projects/index.js
@@ -9,23 +9,24 @@ export default function Projects() {
             <h2>Projects</h2>
             <div className="carousel">
                 <div id="proj-1-container" className="proj-container carousel-item">
-                    <a className="proj-link" href="https://carebnb-2022.herokuapp.com/" target="_blank" rel="noreferrer">
+                    <a className="proj-link" href="https://carebnb-2022.herokuapp.com" target="_blank" rel="noreferrer">
                         <img className="img img--proj" src={carebnbThumbnail} alt="project thumnail 1" />
                     </a>
                     <div>Carebnb, an Airbnb clone</div>
                 </div>
 
                 <div id="proj-2-container" className="proj-container carousel-item">
-                    <a className="proj-link" href="https://squeal-yelp.herokuapp.com/login" target="_blank" rel="noreferrer">
+                    <a className="proj-link" href="https://squeal-yelp.herokuapp.com" target="_blank" rel="noreferrer">
                         <img className="img img--proj" src={squealThumbnail} alt="project thumnail 2" />
                     </a>
                     <div>Squeal, a Yelp clone</div>
                 </div>
 
                 <div id="proj-3-container" className="proj-container carousel-item">
-                    <a className="proj-link" href="">
+                    <a className="proj-link" href="https://escape-hatch.herokuapp.com" target="_blank" rel="noreferrer">
                         <img className="img img--proj" src={placeholder3} alt="project thumnail 3" />
                     </a>
+                    <div>Hatch, a virtual escape room</div>
                 </div>
             </div>
         </div>
