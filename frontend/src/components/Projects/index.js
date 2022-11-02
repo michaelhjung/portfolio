1ef15dc7aeb5fa5440e441e8fb7b40a4ed85a6bb
Michael Jung <jung.michaelh@gmail.com>
2022-11-01 22:58:05 -0700

    Update project images

--- frontend/src/components/Projects/index.js
+++ frontend/src/components/Projects/index.js
@@ -1,7 +1,4 @@
 import "./Projects.css"
-import carebnbThumbnail from "../../assets/carebnb-thumbnail.png"
-import squealThumbnail from "../../assets/squeal-thumbnail.png"
-import placeholder3 from "../../assets/placeholder-3.jpg"
 
 export default function Projects() {
     return (
@@ -10,21 +7,42 @@ export default function Projects() {
             <div className="carousel">
                 <div id="proj-1-container" className="proj-container carousel-item">
                     <a className="proj-link" href="https://carebnb-2022.herokuapp.com" target="_blank" rel="noreferrer">
-                        <img className="img img--proj" src={carebnbThumbnail} alt="project thumnail 1" />
+                        <img
+                        id="carebnb-img"
+                        className="img img--proj"
+                        src="https://i.imgur.com/NT3BnP3.gif"
+                        alt="project thumnail 1"
+                        onMouseOver={e => e.target.src = "https://i.imgur.com/GL5RbON.gif"}
+                        onMouseOut={e => e.target.src = "https://i.imgur.com/NT3BnP3.gif"}
+                    />
                     </a>
                     <div>Carebnb, an Airbnb clone</div>
                 </div>
 
                 <div id="proj-2-container" className="proj-container carousel-item">
                     <a className="proj-link" href="https://squeal-yelp.herokuapp.com" target="_blank" rel="noreferrer">
-                        <img className="img img--proj" src={squealThumbnail} alt="project thumnail 2" />
+                        <img
+                        id="squeal-img"
+                        className="img img--proj"
+                        src="https://i.imgur.com/ybZFSqq.gif"
+                        alt="project thumnail 2"
+                        onMouseOver={e => e.target.src = "https://i.imgur.com/HoTKQgn.gif"}
+                        onMouseOut={e => e.target.src = "https://i.imgur.com/ybZFSqq.gif"}
+                    />
                     </a>
                     <div>Squeal, a Yelp clone</div>
                 </div>
 
                 <div id="proj-3-container" className="proj-container carousel-item">
                     <a className="proj-link" href="https://escape-hatch.herokuapp.com" target="_blank" rel="noreferrer">
-                        <img className="img img--proj" src={placeholder3} alt="project thumnail 3" />
+                        <img
+                        id="hatch-img"
+                        className="img img--proj"
+                        src="https://i.imgur.com/a5sJ7qJ.gif"
+                        alt="project thumnail 3"
+                        onMouseOver={e => e.target.src = "https://i.imgur.com/rPdmTPd.gif"}
+                        onMouseOut={e => e.target.src = "https://i.imgur.com/a5sJ7qJ.gif"}
+                    />
                     </a>
                     <div>Hatch, a virtual escape room</div>
                 </div>
