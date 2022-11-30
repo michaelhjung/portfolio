b2e0af879d08e6e08400974bef9c1fa4e0a35c66
Michael Jung <jung.michaelh@gmail.com>
2022-11-30 12:28:03 -0800

    Update resume link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,7 +6,7 @@ export default function Resume() {
         <section className="resume-container" id="resume">
             <h2>Resume</h2>
             <div className="resume-icons-container">
-                <a className="resume-icon" href="https://drive.google.com/uc?export=download&id=1Ftayh7zYrds7lW0LIamh8yKADQEJ3rU6">
+                <a className="resume-icon" href="https://drive.google.com/uc?export=download&id=1ZP7a5rTdi_6csfpd4whFOD9NLPm7PWPA">
                     <img className="resume-img" src={resume} alt="resume" />
                 </a>
             </div>
